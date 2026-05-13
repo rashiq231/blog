0c4535c33ab88b187e7e5f173e16275092230add
Rashiq <rashiq@Rashiqs-MacBook-Air.local>
2026-05-13 11:57:55 +0530

    fix(workflow): upgrading mongo memory

--- backend/src/test/globalSetup.js
+++ backend/src/test/globalSetup.js
@@ -3,7 +3,7 @@ import { MongoMemoryServer } from 'mongodb-memory-server'
 export default async function gloablSetup() {
   const instance = await MongoMemoryServer.create({
     binary: {
-      version: '6.0.4',
+      version: '7.0.3',
     },
   })
   global.__MONGOINSTANCE = instance
